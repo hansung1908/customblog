bf7a834baa1ae19e1140a4d0a75807f4c63561d3
hansung1908 <hansung1908@naver.com>
2023-06-16 23:43:43 +0900

    프로젝트 생성

---
+++ src/test/java/com/hansung/customblog/CustomblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansung.customblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
