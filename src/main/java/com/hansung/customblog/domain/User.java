f6f5bd69a4efa15ffeb27df713d74b7b5a061a8e
hansung1908 <hansung1908@naver.com>
2023-06-18 23:08:15 +0900

    설정 및 회원정보 저장(실패)

---
+++ src/main/java/com/hansung/customblog/domain/User.java
@@ -0,0 +1,34 @@
+package com.hansung.customblog.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @CreationTimestamp
+    private Timestamp CreateDate;
+}
