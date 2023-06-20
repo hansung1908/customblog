5850a29e54e02a305cdcde45a62f65a259797f59
hansung1908 <hansung1908@naver.com>
2023-06-20 22:41:44 +0900

    블로그 화면 구현

--- src/main/java/com/hansung/customblog/controller/UserController.java
+++ src/main/java/com/hansung/customblog/controller/UserController.java
@@ -1,4 +1,18 @@
 package com.hansung.customblog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/user/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/user/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
 }
