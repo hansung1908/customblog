7775abdb511c0367e68f6d9692ffcbd0871d2583
hansung1908 <hansung1908@naver.com>
2023-06-23 01:50:04 +0900

    spring security을 이용한 로그인 화면 및 기능 구현

--- src/main/java/com/hansung/customblog/repository/UserRepository.java
+++ src/main/java/com/hansung/customblog/repository/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // user 테이블에 pk는 integer
 // 자동으로 bean 등록, 그래서 @Repository 생략
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // select * from user where username = ?1 and password = ?2;
-    User findByUsernameAndPassword(String username, String password);
+
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,10 +3,6 @@ let index = {
         $("#btn-save").on("click", ()=>{
             this.save();
         });
-
-        $("#btn-login").on("click", ()=>{
-            this.login();
-        });
     },
 
     save: function(){
@@ -19,32 +15,12 @@ let index = {
         // ajax를 이용한 비동기 호출 활용
         $.ajax({
             type: "POST",
-            url: "/api/user",
+            url: "/auth/joinProc",
             data: JSON.stringify(data), // http body 데이터
             contentType: "application/json; charset=utf-8", // 요청 타입
             dataType: "json" // 응답 타입
         }).done(function(resp){
             alert("회원가입이 완료되었습니다.");
-            location.href = "/";
-        }).fail(function(error){
-            alert(JSON.stringify(error));
-        });
-    },
-
-    login: function(){
-        let data = {
-            username: $("#username").val(),
-            password: $("#password").val()
-        }
-
-        $.ajax({
-            type: "POST",
-            url: "/api/user/login",
-            data: JSON.stringify(data),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json"
-        }).done(function(resp){
-            alert("로그인이 완료되었습니다.");
             location.href = "/";
         }).fail(function(error){
             alert(JSON.stringify(error));
