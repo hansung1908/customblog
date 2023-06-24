d6945b4f88a1555046ff0e6307d2414de5912ff6
hansung1908 <hansung1908@naver.com>
2023-06-25 02:35:01 +0900

    비밀번호 해쉬 후 회원가입 기능 구현 및 로그인 기능 수정

--- src/main/java/com/hansung/customblog/config/SecurityConfig.java
+++ src/main/java/com/hansung/customblog/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.hansung.customblog.config;
 
+import com.hansung.customblog.config.auth.PrincipalDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,16 +18,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig{
 
+    @Autowired
+    private PrincipalDetailService principalDetailService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authroize -> authroize.requestMatchers("/auth/**")
+                .authorizeHttpRequests(authroize -> authroize.requestMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-                .formLogin(formlogin -> formlogin.loginPage("/auth/loginForm"));
+                .formLogin(formlogin -> formlogin.loginPage("/auth/loginForm")
+                        .loginProcessingUrl("/auth/loginProc")
+                        .defaultSuccessUrl("/"));
 
         return http.build();
     }
-
 }
--- src/main/java/com/hansung/customblog/repository/UserRepository.java
+++ src/main/java/com/hansung/customblog/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.hansung.customblog.repository;
 import com.hansung.customblog.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // user 테이블에 pk는 integer
 // 자동으로 bean 등록, 그래서 @Repository 생략
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    Optional<User> findByUsername(String username);
 }
