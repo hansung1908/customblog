2b45ceeeaecca4382481d34c0b51b371f4dc7523
hansung1908 <hansung1908@naver.com>
2023-07-01 23:47:20 +0900

    카카오 로그인을 위한 인가 코드 및 토큰 요청 (서비스 미완성)

--- src/main/java/com/hansung/customblog/controller/UserController.java
+++ src/main/java/com/hansung/customblog/controller/UserController.java
@@ -1,14 +1,55 @@
 package com.hansung.customblog.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.hansung.customblog.service.OAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private OAuthService oAuthService;
+
     @GetMapping("/auth/loginForm")
     public String loginForm() {
         return "user/loginForm";
+    }
+
+    @GetMapping("/auth/kakao/callback")
+    public @ResponseBody String kakaoCallback(String code) throws JsonProcessingException {
+        // post방식으로 key=value 데이터를 요청 (카카오쪽으로)
+        RestTemplate rt = new RestTemplate();
+
+        // http header 오브젝트 생성
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        // http body 오브젝트 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code");
+        params.add("client_id", "87d8174de120aa789c94d20377cea7e6");
+        params.add("redirect_uri", "http://localhost:8080/auth/kakao/callback");
+        params.add("code", code);
+
+        // header와 body 통합
+        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest = new HttpEntity<>(params, headers);
+
+        // 1. 토큰 발급 주소, 2. 요청 메소드 타입, 3. http 데이터, 4. 응답 타입
+        ResponseEntity<String> response = rt.exchange("https://kauth.kakao.com/oauth/token", HttpMethod.POST, kakaoTokenRequest, String.class);
+
+        ResponseEntity<String> profileResponse = oAuthService.token(response);
+
+        return profileResponse.getBody();
     }
 
     @GetMapping("/auth/joinForm")
