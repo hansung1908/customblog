9a5f7d85ca572edc4f3b4aff4d3e399279d6d1af
hansung1908 <hansung1908@naver.com>
2023-07-03 23:51:50 +0900

    카카오 로그인을 통한 자동 회원가입 및 자동 로그인 기능 구현 (서비스 미완성)

--- src/main/java/com/hansung/customblog/service/OAuthService.java
+++ src/main/java/com/hansung/customblog/service/OAuthService.java
@@ -2,11 +2,20 @@ package com.hansung.customblog.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hansung.customblog.model.KakaoProfile;
 import com.hansung.customblog.model.OAuthToken;
+import com.hansung.customblog.model.User;
+import com.hansung.customblog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.MultiValueMap;
@@ -14,6 +23,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OAuthService {
+
+    @Value("${hansung.key}")
+    private String key;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Transactional
     public ResponseEntity<String> token(ResponseEntity<String> response) throws JsonProcessingException {
@@ -29,6 +50,29 @@ public class OAuthService {
         HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest = new HttpEntity<>(headers);
 
         ResponseEntity<String> profileResponse = rt.exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST, kakaoProfileRequest, String.class);
+
+        KakaoProfile kakaoProfile = ob.readValue(profileResponse.getBody(), KakaoProfile.class);
+
+        User kakaoUser = User.builder()
+                .username(kakaoProfile.getKakao_account().getEmail() + " " + kakaoProfile.getId())
+                .password(key)
+                .email(kakaoProfile.getKakao_account().getEmail())
+                .build();
+
+        // 가입자 혹은 비가입자 체크
+        User originUser = userRepository.findByUsername(kakaoUser.getUsername()).orElseGet(() -> {
+            return new User();
+        });
+
+        // 비가입자면 회원가입
+        if(originUser.getUsername() == null) {
+            System.out.println("기존 회원이 아니기에 자동 회원가입을 진행합니다.");
+            userService.save(kakaoUser);
+        }
+
+        // 로그인 처리
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(kakaoUser.getUsername(), key));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
         return profileResponse;
     }
