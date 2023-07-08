2458e93c05970cd774df0911f0a747ee5789b2db
hansung1908 <hansung1908@naver.com>
2023-07-08 23:54:24 +0900

    댓글 관련 기능 구현

--- src/main/java/com/hansung/customblog/controller/api/BoardApiController.java
+++ src/main/java/com/hansung/customblog/controller/api/BoardApiController.java
@@ -1,8 +1,10 @@
 package com.hansung.customblog.controller.api;
 
 import com.hansung.customblog.config.auth.PrincipalDetail;
+import com.hansung.customblog.dto.ReplySaveRequestDto;
 import com.hansung.customblog.dto.ResponseDto;
 import com.hansung.customblog.model.Board;
+import com.hansung.customblog.model.Reply;
 import com.hansung.customblog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,6 +32,18 @@ public class BoardApiController {
     @PutMapping("/api/board/{id}")
     public ResponseDto<Integer> update(@PathVariable int id, @RequestBody Board board) {
         boardService.update(id, board);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
+    @PostMapping("/api/board/{boardId}/reply")
+    public ResponseDto<Integer> replySave(@RequestBody ReplySaveRequestDto replySaveRequestDto) {
+        boardService.replySave(replySaveRequestDto);
+        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
+    }
+
+    @DeleteMapping("/api/board/{boardId}/reply/{replyId}")
+    public ResponseDto<Integer> replyDelete(@PathVariable int replyId) {
+        boardService.replyDelete(replyId);
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
 }
