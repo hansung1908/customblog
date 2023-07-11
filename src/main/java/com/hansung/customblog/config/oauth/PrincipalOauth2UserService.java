314fdac674d9da63a6a6b12f2ed00f7ffc246d7b
hansung1908 <hansung1908@naver.com>
2023-07-11 22:26:45 +0900

    네이버 로그인 기능 구현

--- src/main/java/com/hansung/customblog/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/hansung/customblog/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,9 @@
 package com.hansung.customblog.config.oauth;
 
 import com.hansung.customblog.config.auth.PrincipalDetail;
+import com.hansung.customblog.config.oauth.provider.GoogleUserInfo;
+import com.hansung.customblog.config.oauth.provider.NaverUserInfo;
+import com.hansung.customblog.config.oauth.provider.OAuth2UserInfo;
 import com.hansung.customblog.model.RoleType;
 import com.hansung.customblog.model.User;
 import com.hansung.customblog.repository.UserRepository;
@@ -12,6 +15,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
@@ -31,12 +36,23 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if(userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        } else if(userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+        } else {
+            System.out.println("구글과 네이버만 지원");
+        }
+
         // 자동 회원가입
-        String provider = userRequest.getClientRegistration().getRegistrationId(); // google
-        String providerId = oAuth2User.getAttribute("sub");
+        String provider = oAuth2UserInfo.getProvider();
+        String providerId = oAuth2UserInfo.getProviderId();
         String username = provider + "_" + providerId;
         String password = bCryptPasswordEncoder.encode(key);
-        String email = oAuth2User.getAttribute("email");
+        String email = oAuth2UserInfo.getEmail();
 
         User googleUser = userRepository.findByUsername(username).orElseGet(() -> new User());
 
