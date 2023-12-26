f513afe3d7575cd37e3f5fa47c92484eac96ac94
hansung1908 <hansung1908@naver.com>
2023-12-26 22:16:51 +0900

    logback을 활용한 logging 기능 구현

--- src/main/java/com/hansung/customblog/config/BindingAdvice.java
+++ src/main/java/com/hansung/customblog/config/BindingAdvice.java
@@ -4,6 +4,8 @@ import com.hansung.customblog.dto.response.ResponseDto;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
@@ -16,9 +18,12 @@ import java.util.Map;
 @Aspect
 public class BindingAdvice {
 
+    private static final Logger log = LoggerFactory.getLogger(BindingAdvice.class);
+
     // controller 패키지의 모든 Controller가 붙는 클래스의 인수 갯수는 상관 없이 모든 메소드에 적용
     @Around("execution(* com.hansung.customblog.controller..*Controller.*(..))")
     public Object vaildCheck(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+
         String type = proceedingJoinPoint.getSignature().getDeclaringTypeName();
         String method = proceedingJoinPoint.getSignature().getName();
 
@@ -36,6 +41,10 @@ public class BindingAdvice {
 
                     for(FieldError error : bindingResult.getFieldErrors()) {
                         errorMap.put(error.getField(), error.getDefaultMessage());
+                        // 로그 레벨: error < warn < info < debug (레벨이 클수록 하위 로그를 출력, 예: info 레벨이면 error, warn, info만 출력)
+                        log.warn(type + "." + method + "() -> 필드: " + error.getField() + ", 메세지: " + error.getDefaultMessage());
+                        // 로그를 파일로 생성하면 관리가 힘들어 진다.
+                        log.debug(type + "." + method + "() -> 필드: " + error.getField() + ", 메세지: " + error.getDefaultMessage());
                     }
 
                     return new ResponseDto<>(HttpStatus.BAD_REQUEST.value(), errorMap);
