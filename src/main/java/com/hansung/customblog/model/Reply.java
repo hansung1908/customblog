1db852ff66c41d5b5a9e6cb64915cf93b7f0a856
hansung1908 <hansung1908@naver.com>
2024-08-03 23:01:52 +0900

    lombok 생성자 어노테이션 변경

--- src/main/java/com/hansung/customblog/model/Reply.java
+++ src/main/java/com/hansung/customblog/model/Reply.java
@@ -1,20 +1,28 @@
 package com.hansung.customblog.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
 
 @Entity
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Builder
 public class Reply {
+
+    protected Reply() {
+    }
+
+    public Reply(int id, String content, Board board, User user, Timestamp createDate) {
+        this.id = id;
+        Content = content;
+        this.board = board;
+        this.user = user;
+        this.createDate = createDate;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
