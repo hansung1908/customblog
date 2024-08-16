22b59876ed1ae8e14d1b841765db1618cffd99b1
hansung1908 <hansung1908@naver.com>
2024-08-17 00:00:17 +0900

    자동 로그인 기능 추가 (테스트 오류 발생)

---
+++ src/main/java/com/hansung/customblog/config/TokenRepository.java
@@ -0,0 +1,24 @@
+package com.hansung.customblog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class TokenRepository {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PersistentTokenRepository createTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setCreateTableOnStartup(true);
+        jdbcTokenRepository.setDataSource(dataSource);
+        return jdbcTokenRepository;
+    }
+}
