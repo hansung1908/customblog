51b40f620c6352bd4bf5350dfb0c467f91938e3a
hansung1908 <hansung1908@naver.com>
2024-08-18 01:05:57 +0900

    자동 로그인 기능 추가 (테스트 통과)

--- src/main/java/com/hansung/customblog/config/TokenRepository.java
+++ src/main/java/com/hansung/customblog/config/TokenRepository.java
@@ -12,13 +12,13 @@ import javax.sql.DataSource;
 public class TokenRepository {
 
     @Autowired
-    private DataSource dataSource;
+    private DataSource dataSource; // application.yml의 datasource를 객체로 가져옴
 
     @Bean
-    public PersistentTokenRepository createTokenRepository() {
-        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
-        jdbcTokenRepository.setCreateTableOnStartup(true);
-        jdbcTokenRepository.setDataSource(dataSource);
+    public PersistentTokenRepository setupTokenRepositoryConnection() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl(); // 토큰 저장 db를 등록하는 객체
+        jdbcTokenRepository.setCreateTableOnStartup(false); // 토큰에 대한 테이블 생성 유무, 한번 생성했으면 false로 변경
+        jdbcTokenRepository.setDataSource(dataSource); // 테이블을 연결하기 위한 데이터소스 (db driver, db url, 아이디, 비번)
         return jdbcTokenRepository;
     }
 }
