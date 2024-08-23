10579e0bf124e3dd23f59a6504574902f17f772d
hansung1908 <hansung1908@naver.com>
2024-08-23 22:44:52 +0900

    파일 게시판 표시 완성 (다운로드 x)

--- src/main/java/com/hansung/customblog/repository/FileRepository.java
+++ src/main/java/com/hansung/customblog/repository/FileRepository.java
@@ -3,5 +3,9 @@ package com.hansung.customblog.repository;
 import com.hansung.customblog.model.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FileRepository extends JpaRepository<File, Integer> {
+
+    Optional<File> findByBoardId(int id);
 }
