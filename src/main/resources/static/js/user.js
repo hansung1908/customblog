fcc80a66f96d0b6ec5b1a914de39aba5168d817c
hansung1908 <hansung1908@naver.com>
2024-08-31 23:11:20 +0900

    회원가입 유저이름 중복체크 프론트엔드 완성 (백엔드 완성 x) + 불필요한 div 삭제

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -7,6 +7,10 @@ let index = {
         $("#btn-update").on("click", ()=>{
             this.update();
         });
+
+        $("#btn-checkName").on("click", ()=>{
+            this.checkName();
+        });
     },
 
     save: function(){
@@ -57,6 +61,27 @@ let index = {
             if (resp.status === 200) {
                 alert(JSON.stringify(resp.data));
                 location.href = "/";
+            } else {
+                alert(JSON.stringify(resp.data));
+            }
+        });
+    },
+
+    checkName: function(){
+        let username = $("#username").val();
+
+        $.ajax({
+            type: "POST",
+            url: "/auth/joinProc/checkName",
+            data: JSON.stringify(username),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json"
+        }).always(function(resp) {
+            console.log("HTTP Status Code: " + resp.status);
+            console.log("Response Text: ", resp.data);
+
+            if (resp.status === 200) {
+                alert(JSON.stringify(resp.data));
             } else {
                 alert(JSON.stringify(resp.data));
             }
