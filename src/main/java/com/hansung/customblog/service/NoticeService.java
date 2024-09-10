c0cd1945a24602f2f796e499fac166f59eb85500
hansung1908 <hansung1908@naver.com>
2024-09-11 02:42:03 +0900

    어드민 페이지 공지사항 전체 목록 표시 부분 완성 (삭제, 페이징 x)

--- src/main/java/com/hansung/customblog/service/NoticeService.java
+++ src/main/java/com/hansung/customblog/service/NoticeService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 public class NoticeService {
 
@@ -27,5 +29,10 @@ public class NoticeService {
     @Transactional
     public Notice findLatestNotice() {
         return noticeRepository.findLatestNotice();
+    }
+
+    @Transactional
+    public List<Notice> findAll() {
+        return noticeRepository.findAll();
     }
 }
