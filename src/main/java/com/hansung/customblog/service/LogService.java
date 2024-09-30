6c36f8fadb0d392fb212dd799f786e99252b3ada
hansung1908 <hansung1908@naver.com>
2024-09-30 22:48:36 +0900

    어드민 페이지 오류 로그 표시 추가

---
+++ src/main/java/com/hansung/customblog/service/LogService.java
@@ -0,0 +1,27 @@
+package com.hansung.customblog.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LogService {
+
+    public List<String> readTodayLog(String filePath) {
+        List<String> logLines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                logLines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return logLines;
+    }
+}
