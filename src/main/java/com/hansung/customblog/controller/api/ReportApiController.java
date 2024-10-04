04cb9fa0ae9fc4f3e6e4221e4398605f14cef85b
hansung1908 <hansung1908@naver.com>
2024-10-04 23:57:40 +0900

    report 프론트엔드 제작중 (오류 발생)

---
+++ src/main/java/com/hansung/customblog/controller/api/ReportApiController.java
@@ -0,0 +1,18 @@
+package com.hansung.customblog.controller.api;
+
+import com.hansung.customblog.dto.request.ReportSaveRequestDto;
+import com.hansung.customblog.dto.response.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
+public class ReportApiController {
+
+    @PostMapping("/api/report")
+    public ResponseDto<String> save(@RequestBody ReportSaveRequestDto reportSaveRequestDto) {
+        System.out.println(reportSaveRequestDto.getReason());
+        return new ResponseDto<String>(HttpStatus.OK.value(), "게시글 저장이 완료되었습니다.");
+    }
+}
